72e41510c9bf391eae5caa9abd111af898f06e06
ket4yii <ket4yiit@gmail.com>
2016-10-20 23:36:30 +0300

    Change error handling

--- musfrompost.sh
+++ musfrompost.sh
@@ -26,13 +26,14 @@ api_url="https://api.vk.com/method/wall.getById?posts=$post_id&extended=0&v=5.52
 audios_json=$(curl $api_url 2> /dev/null | jq '.response[0].copy_history[0] // .response[0] | .attachments | map(select(.type == "audio") | .audio | {artist, title, url})')
 len=$(echo $audios_json | jq 'length')
 ((len--))
+echo $len;
 
 for i in `seq 0 $len`; do
     title=$(echo $audios_json | jq -r ".[$i].title")
     artist=$(echo $audios_json | jq -r ".[$i].artist")
     url=$(echo $audios_json | jq -r ".[$i].url")
     
-    echo $artist -- $title
-    wget -q --show-progress -O "$folder/$artist-$title" "$url"
+    echo "$artist -- $title"
+    wget -q --show-progress -O "$folder/$artist-$title" "$url" || echo "Can't download this track. Check this link manually: $url"
 done
 
